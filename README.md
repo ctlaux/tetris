ef2a31efdc0aa08197f393879ed9474e73faa055
Chris Laux <ctlaux@gmail.com>
2013-07-17 17:31:48 +0200

    Added GPL license text

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-tetris
-======
+Inspectable Tetris clone 
+
+Version 0.1
+
+Copyright (c) 2013 Christopher Laux <ctlaux@gmail.com>
+
+Licensed under the GNU General Public License v3
+
+Move brick about using the left and right arrow. Drop by holding the down arrow. Rotate counter-clockwise by pressing the up arrow. Pause with the 'p' key.
+
--- inspector.js
+++ inspector.js
@@ -1,3 +1,19 @@
+// Inspectable Tetris clone 
+// Copyright (c) 2013 Christopher Laux <ctlaux@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 var code_parts = ["rotate_brick", "check_if_stopped", "remove_full_lines"];
 var arrows = {"rotate_brick": {"brick_array": true, "rotation": true, "squares": false, "field_xy": false, "field_squares": false},
