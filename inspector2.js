d02905dc585508b48f6d34e1583d01f6c32ebc73
Chris Laux <ctlaux@gmail.com>
2013-08-07 20:59:52 +0200

    Added first layer of mouse selection

--- inspector2.js
+++ inspector2.js
@@ -20,6 +20,9 @@ var xml;
 
 var current_selection = null;
 
+var rotation_x = 100;
+var rotation_y = 100;
+
 
 function on_launch() {
 }
@@ -30,7 +33,7 @@ function draw_inspector() {
     var c = canvas2.getContext('2d');
 
     if(current_selection == "rotate_p")
-	draw_rotation(c, 100, 100, true, "red", null, null, null);
+	draw_rotation(c, rotation_x, rotation_y, true, null, null, "red", null);
 }
 
 function code_clicked(obj) {
@@ -61,4 +64,33 @@ function render_markup(code) {
     }
 
     $("#code").append(html);
+}
+
+function click_select_square_canvas2(e) {
+    var canvas2 = document.getElementById('canvas2');
+    var cursor = get_cursor_position(canvas2, e);
+    var mouse_x = cursor[0];
+    var mouse_y = cursor[1];
+    var change = false;
+
+    if(current_selection != null && current_selection.substring(0, 7) == "rotate_") {
+	var b = calculate_squares(0, type);
+
+	for(var i = 0; i < b.length; i++)
+	    if(click_inside_square(mouse_x, mouse_y, rotation_x, rotation_y, b[i])) {
+		make_selection(i);
+		change = true;
+		break;
+	    }
+
+	for(var i = 0; i < squares.length; i++)
+	    if(click_inside_square(mouse_x, mouse_y, rotation_x + 100, rotation_y, squares[i])) {
+		make_selection(i);
+		change = true;
+		break;
+	    }
+    }
+
+    if(change)
+	draw_inspector();
 }
